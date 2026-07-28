7f3daa3da81b177ae4bdc733b7f40fc120792e19
Guilherme Ryan <guilhermeryan277@gmail.com>
2026-07-27 21:46:21 -0300

    feat: adiciona funcionalidade de editar e excluir produtos

--- produtos/urls.py
+++ produtos/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', views.produtos, name='produtos'),
     path('novo', views.adicionar_produto, name='novo'),
     path('<int:produto_id>', views.detalhes_produto, name='detalhes'),
-    path('<int:produto_id>/editar', views.detalhes_produto, name='detalhes'),
-    path('<int:produto_id>/excluir', views.detalhes_produto, name='detalhes'),
+    path('<int:produto_id>/editar', views.editar_produto, name='editar'),
+    path('<int:produto_id>/excluir', views.excluir_produto, name='excluir'),
 ]
--- produtos/views.py
+++ produtos/views.py
@@ -19,9 +19,25 @@ def adicionar_produto(request):
             preco = preco,
             quantidade = quantidade
         )
-        return redirect('produtos')
+        return redirect('produtos:produtos')
     return render(request, 'produtos/adicionar_produtos.html')
+
+def editar_produto(request, produto_id):
+    produto = Produto.objects.get(id=produto_id)
+    if request.method == 'POST':
+        produto.nome = request.POST.get('nome')
+        produto.descricao = request.POST.get('descricao')
+        produto.preco = request.POST.get('preco')
+        produto.quantidade = request.POST.get('quantidade')
+        produto.save()
+        return redirect('produtos:detalhes', produto.id)
+    return render(request, 'produtos/editar_produto.html', {'produto': produto})
 
 def detalhes_produto(request, produto_id):
     produto = Produto.objects.get(id = produto_id)
     return render(request, 'produtos/detalhes_produto.html', {'produto': produto})
+
+def excluir_produto(request, produto_id):
+    produto = Produto.objects.get(id = produto_id)
+    produto.delete()
+    return redirect('produtos:produtos')

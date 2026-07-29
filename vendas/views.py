c3f117e74ccb2393647a9c8249410726a740f566
Guilherme Ryan <guilhermeryan277@gmail.com>
2026-07-29 11:42:53 -0300

    feat: adiciona funcionalidades de cancelamento e detalhamento de vendas

--- vendas/views.py
+++ vendas/views.py
@@ -1,15 +1,41 @@
-from django.shortcuts import render
-from .models import Venda
+from django.shortcuts import render, redirect
+from .models import Venda, Produto, Cliente
 
 def vendas(request):
     vendas = Venda.objects.all()
     return render(request, 'vendas/ver_vendas.html', {'vendas':vendas})
 
 def adicionar_venda(request):
-    pass
+    clientes = Cliente.objects.all()
+    produtos = Produto.objects.all()
+    context = {'clientes': clientes, 'produtos': produtos}
+    if request.method == 'POST':
+        cliente = Cliente.objects.get(id = request.POST.get('cliente'))
+        produto = Produto.objects.get(id = request.POST.get('produto'))
+        quantidade = int(request.POST.get('quantidade'))
+        if quantidade <= 0:
+            context['erro'] = 'A quantidade deve ser maior que zero.'
+            return render(request, 'vendas/adicionar_vendas.html', context)
+        if quantidade > produto.quantidade:
+            context['erro'] = 'Estoque insuficiente.'
+            return render(request, 'vendas/adicionar_vendas.html', context)
+        valor_total = produto.preco * quantidade
+        Venda.objects.create(
+            cliente = cliente,
+            produto = produto,
+            quantidade = quantidade,
+            valor_total = valor_total
+        )
+        produto.quantidade -= quantidade
+        produto.save()
+        return redirect('vendas:vendas')
+    return render(request, 'vendas/adicionar_vendas.html', context)
 
-def detalhes_venda(request):
-    pass
+def detalhes_venda(request, venda_id):
+    venda = Venda.objects.get(id = venda_id)
+    return render(request, 'vendas/detalhes_venda', {'venda':venda})
 
-def excluir_venda(request):
-    pass
+def excluir_venda(request, venda_id):
+    venda = Venda.objects.get(id = venda_id)
+    venda.delete()
+    return redirect('vendas:vendas')

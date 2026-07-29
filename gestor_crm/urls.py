4f0788239e60c1ffef7ce6f058c4154ece48a00c
Guilherme Ryan <guilhermeryan277@gmail.com>
2026-07-29 11:43:50 -0300

    feat: adiciona uma url que aponta para a pagina inicial de vendas

--- gestor_crm/urls.py
+++ gestor_crm/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('clientes/', include('clientes.urls')),
     path('produtos/', include('produtos.urls')),
+    path('vendas/', include('vendas.urls')),
 ]

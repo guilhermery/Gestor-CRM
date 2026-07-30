d0cd87f92f5f6412155cb45cb6661b8c3db2ac3a
Guilherme Ryan <guilhermeryan277@gmail.com>
2026-07-29 23:00:42 -0300

    refactor: corrige erro de nome de variavel id

--- vendas/urls.py
+++ vendas/urls.py
@@ -7,6 +7,6 @@ app_name = "vendas"
 urlpatterns = [
     path('', views.vendas, name='vendas'),
     path('novo', views.adicionar_venda, name='novo'),
-    path('<int:produto_id>', views.detalhes_venda, name='detalhes'),
-    path('<int:produto_id>/excluir', views.excluir_venda, name='excluir'),
+    path('<int:venda_id>', views.detalhes_venda, name='detalhes'),
+    path('<int:venda_id>/excluir', views.excluir_venda, name='excluir'),
 ]

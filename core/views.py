959b4fa8c99f65fb543abb1052897a945c93dbb4
Guilherme Ryan <guilhermeryan277@gmail.com>
2026-07-29 23:02:14 -0300

    feat: adiciona funcionalidades e template da página inicial do CRM

--- core/views.py
+++ core/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Produto, Cliente, Venda
 
 def index(request):
-    return render(request, 'core/index.html')
+    clientes = Cliente.objects.all()
+    produtos = Produto.objects.all()
+    vendas = Venda.objects.all()
+
+    total_clientes = clientes.count()
+    total_produtos = produtos.count()
+    total_vendas = vendas.count()
+
+    faturamento = 0.0
+
+    for venda in vendas:
+        faturamento += venda.valor_total
+
+    estoque_baixo = Produto.objects.filter(quantidade__lte=5)
+    context = {'total_clientes': total_clientes, 'total_produtos': total_produtos, 'total_vendas': total_vendas, 'faturamento': faturamento, 'estoque_baixo': estoque_baixo}
+    return render(request, 'core/index.html', context)
